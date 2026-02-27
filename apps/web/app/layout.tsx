3b89e2532da95051dbbc02967511e7133c2d6691
Mahmudul Hasan <mahmudul.dev7@gmail.com>
2026-02-27 12:49:54 +0600

    feat: init web

---
+++ apps/web/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const fontSans = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const metadata: Metadata = {
+  title: "NestDrive",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={fontSans.className}>{children}</body>
+    </html>
+  );
+}
